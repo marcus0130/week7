646f668faee0383dadc063db959faca6708e8e66
marcus0130 <91177504+marcus0130@users.noreply.github.com>
2023-10-25 16:42:53 +0800

    ww

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './components/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import OfficeInformation from './components/OfficeInformation'
+import OfficeInformation from "./components/office"
 function App() {
   
   return (
