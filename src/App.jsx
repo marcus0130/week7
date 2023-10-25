6500b1e86b1c0435cb11566eaa3aad3590a5f87e
marcus0130 <91177504+marcus0130@users.noreply.github.com>
2023-10-25 16:21:19 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './components/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import OfficeInformation from './components/OfficeInformation'
+function App() {
+  
+  return (
+  <>
+   <OfficeInformation
+      imgLink="https://econ.ntpu.edu.tw/storage/images/ZlP7DHjLRh8IeahmFoT1EMxkmCgaxYtguN76FqiW.jpg"
+      name = "郭文宗 教授"
+      officeHour="Thu. 12:00 - 14:00"
+      email="guowc@gm.ntpu.edu.tw"
+      phone="886-2-86741111 ext. 67156"
+      office="社3F16"          
+                />
+  </>
+  )
+}
+
+export default App
+
